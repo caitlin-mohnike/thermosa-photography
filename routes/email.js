987006a01fb0be4dc873fdfbb35a9f3150d8f4c7
Caitlin <ccmohnike@gmail.com>
2019-04-04 11:16:28 -0700

    UI changes and updates

---
+++ routes/email.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var Email = require('email').Email
+
+router.post('/send-email', function(req, res) {
+  
+});
+
+module.exports = router;

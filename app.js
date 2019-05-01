5fd2e3390bd83101fb9a027eb30364b7dad9990b
Caitlin <ccmohnike@gmail.com>
2019-05-01 12:06:20 -0700

    forgot a line to remove the routing to https

--- app.js
+++ app.js
@@ -31,15 +31,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/email', emailRouter);
 
-app.use (function (req, res, next) {
-    if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-    } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+// app.use (function (req, res, next) {
+//     if (req.secure) {
+//         // request was via https, so do no special handling
+//         next();
+//     } else {
+//         // request was via http, so redirect to https
+//         res.redirect('https://' + req.headers.host + req.url);
+//     }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
